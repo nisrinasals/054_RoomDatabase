ea328b05740d3cceeefeea9a344be160c957735a
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-27 16:06:29 +0700

    membuat companion object

--- app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
@@ -9,6 +9,11 @@ import androidx.room.RoomDatabase
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
 abstract class DatabaseSiswa: RoomDatabase(){
     abstract fun siswaDao(): SiswaDao
+    companion object{
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
 
+
+    }
 
 }
