380da238e8580e8e8e985fa5b021c621ef6f55e8
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-27 16:13:51 +0700

    membuat fun Siswa.toDetailSiswa()

--- app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
@@ -54,3 +54,10 @@ fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateS
     detailSiswa = this.toDetailSiswa(),
     isEntryValid = isEntryValid
 )
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
