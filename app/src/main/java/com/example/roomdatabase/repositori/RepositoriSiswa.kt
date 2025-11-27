fe51fee12476ce3a48f615669010d9eb2d879816
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-27 16:15:00 +0700

    membuat RepositoriSiswa.kt

---
+++ app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
@@ -0,0 +1,19 @@
+package com.example.roomdatabase.repositori
+
+import com.example.roomdatabase.room.Siswa
+import com.example.roomdatabase.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa{
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao
+        .getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao
+        .insert(siswa)
+}
