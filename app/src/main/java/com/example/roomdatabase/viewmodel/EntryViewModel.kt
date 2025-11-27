4171c5b6398bc0ca85b7362db699a2f23ef163a7
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-27 16:11:45 +0700

    membuat suspend fun saveSiswa

--- app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
@@ -23,5 +23,10 @@ class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel()
                 isEntryValid = validasiInput(detailSiswa))
     }
 
-
+    suspend fun saveSiswa() {
+        if (validasiInput()) {
+            repositoriSiswa.insertSiswa(uiStateSiswa
+                .detailSiswa.toSiswa())
+        }
+    }
 }
